6a7b50680a97c8e6932f1e5ceb89d84f2a5748d3
Pushti <pushtikadia@gmail.com>
2025-12-09 19:42:13 +0530

    Done Demo-1.py of (Pandas Join DataFrame) With Some Updates

---
+++ Panda Series Attribute Methods/Demo-1.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# Data to be stored in the Pandas Series
+data = [10, 20, 40, 80, 100]
+
+# Create a Series using the Series() method
+s = pd.Series(data)
+
+# Display the Series
+print("Series: \n", s)
+
+# Datatype
+print("\nSeries Datatype: ", s.dtype)
